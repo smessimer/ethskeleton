9d8e47fadfd5a7caf917c265e2aa49bbd290fffb
seth <sethmessimer@gmail.com>
2017-07-23 19:07:52 -0500

    Added default account generation to gen.sh

--- gen.sh
+++ gen.sh
@@ -1,3 +1,7 @@
 geth --ipcdisable --nodiscover --rpc --rpcport "8081" --datadir ./01 --port "30311" --identity "Node01" init CustomGenesis.json
 
 geth --ipcdisable --nodiscover --rpc --rpcport "8082" --datadir ./02 --port "30312" --identity "Node02" init CustomGenesis.json
+
+geth --datadir ./01 account new
+
+geth --datadir ./02 account new
